62ceacad0ac963d58cc3d16ca9d2e66941fc74aa
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-04-18 04:40:56 +0200

    init.sh: improved Docker priming script

--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
-set -x
+# set -x
 
-if grep -q docker /proc/1/cgroup; then
-	echo ""
+if grep -q docker /proc/1/cgroup || test -f /.dockerenv; then
+	echo "" # "Running in Docker"
 else
 	echo "ERROR: This script is meant to be run in a Docker container! exiting."
 	exit
 fi
 
-apt update && apt install -y ca-certificates && apt dist-upgrade
+apt update && apt install -y ca-certificates && apt dist-upgrade -y
 
 npm i -g pnpm turbo typescript
 
